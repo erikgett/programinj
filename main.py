2c4767508556359bfcbb2dc02a136f2ae1841333
erikgett <erikgette63@gmail.com>
2023-04-25 18:04:41 +0500

    fasdf

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 df = pd.read_csv("data.csv")
 # Вывести имя и возраст спасенных детей, указав пол и возраст (от 0 до 18).
