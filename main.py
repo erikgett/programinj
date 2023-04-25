319bb07868b0ec2f2ea4f7ff31627147f5301149
VictoriaVash <vash-200@mail.ru>
2023-04-25 18:52:24 +0500

    3 комит

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ def erik(df):
     pass
 
 
-def Vika(df):
+def Vika(df, min_age, max_age, sex):
 # Вывести имя и возраст спасенных детей, указав пол и возраст (от 0 до 18).
-    a = df.groupby('Name')['Age'].count()
-    print(a)
+    if sex == 0: sex = 'male'
+    else: sex = 'female'
+    young_survived = df[df['Age'] < max_age][df['Age'] >= min_age][df['Sex']==sex]
+    out_table = df.young_survived.Age
+    print(young_survived.Age, young_survived.Sex, young_survived.Name)
 
 if __name__ == "__main__":
     erik(df)
-    Vika(df)
+    Vika(df,5,10,0)
