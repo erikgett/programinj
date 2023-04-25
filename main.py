c3b4acc01cc81d0dbc0c89e02a697581ef067ae4
VictoriaVash <vash-200@mail.ru>
2023-04-25 17:58:43 +0500

    добавила импорты и считала файл

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data.csv")
 # Вывести имя и возраст спасенных детей, указав пол и возраст (от 0 до 18).
+
 # Вывести имя, возраст, класс билета пассажиров, выбрав пол и спасен/нет.
 
