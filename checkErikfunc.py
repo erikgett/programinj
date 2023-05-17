889f3faa4c5972a2f12c11734dfad4efb8753af9
erikgett <erikgette63@gmail.com>
2023-05-17 18:38:20 +0500

    добавил тесты

--- checkErikfunc.py
+++ checkErikfunc.py
@@ -6,8 +6,22 @@ df = pd.DataFrame({
     'PassengerId': [1, 2, 3, 4],
     'Survived': [0, 1, 1, 1],
     'Pclass': [3, 1, 3, 2],
-    'Name': ["Braund, Mr. Owen Harris", "Moran, Mr. James",
-             "Heikkinen, Miss. Laina", "Bonnell, Miss. Elizabeth"],
+    'Name': ["Патрик", "Вика",
+             "Эрик", "Вик"],
+    'Sex': [0, 1, 0, 1],
+    'Age': [14, 16, 12, 26],
+    'SibSp': [0, 0, 0, 0],
+    'Parch': [0, 0, 1, 1],
+    'Ticket': [4, 6, 7, 8],
+    'Fare': [4, 6, 7, 8],
+    'Cabin': [4, 6, 7, 8],
+    'Embarked': [4, 6, 7, 8]})
+df2 = pd.DataFrame({
+    'PassengerId': [1, 2, 3, 4],
+    'Survived': [0, 1, 1, 1],
+    'Pclass': [3, 1, 3, 2],
+    'Name': ["Патрик", "Вика",
+             "Эрик", "Вик"],
     'Sex': [0, 1, 0, 1],
     'Age': [14, 16, 12, 26],
     'SibSp': [0, 0, 0, 0],
@@ -17,6 +31,5 @@ df = pd.DataFrame({
     'Cabin': [4, 6, 7, 8],
     'Embarked': [4, 6, 7, 8]})
 
-
 def test_erik_func():
-    assert (True)
+    assert (erik(df, 1, 1).equals(erik(df, 1, 1)))
