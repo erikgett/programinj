fdce1166f79bf8110431180f16eb6450d43af0bb
erikgett <erikgette63@gmail.com>
2023-05-17 18:20:44 +0500

    добавил тесты

--- checkErikfunc.py
+++ checkErikfunc.py
@@ -1,5 +1,22 @@
 from main import erik
+import pandas as pd
+
+
+df = pd.DataFrame({
+    'PassengerId': [1, 2, 3, 4],
+    'Survived': [0, 1, 1, 1],
+    'Pclass': [3, 1, 3, 2],
+    'Name': ["Braund, Mr. Owen Harris", "Moran, Mr. James",
+             "Heikkinen, Miss. Laina", "Bonnell, Miss. Elizabeth"],
+    'Sex': [0, 1, 0, 1],
+    'Age': [14, 16, 12, 26],
+    'SibSp': [22, 38, 35, 37],
+    'Parch': [4, 6, 7, 8],
+    'Ticket': [4, 6, 7, 8],
+    'Fare': [4, 6, 7, 8],
+    'Cabin': [4, 6, 7, 8],
+    'Embarked': [4, 6, 7, 8]})
 
 
 def test_erik_func():
-    pass
+    assert (erik(pd.read_csv("data.csv"), 0, 0).equals(erik(pd.read_csv("data.csv"), 0, 0)))
